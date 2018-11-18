b15d10a5d8c758025ab2a97907a92eea55de286f
Hdonge <harshal.donge@gmail.com>
2018-11-18 11:38:25 +0530

    React js events

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,38 +4,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  btnClick() {
+    console.log("button clicked................");
+  }
   render() {
     return (
       <div>
-        <h1>Hello, {this.props.name}</h1>
-        <h3>Array: {this.props.propArray}</h3>
-        <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
-        <h3>Func: {this.props.propFunc(3)}</h3>
-        <h3>Number: {this.props.propNumber}</h3>
-        <h3>String: {this.props.propString}</h3>
+        <p>Here is the button</p>
+        <button onClick={this.btnClick}>Click Here</button>
       </div>
     );
   }
 }
-
-App.propTypes = {
-  name: PropTypes.string,
-  propArray: PropTypes.isRequired,
-  propBool: PropTypes.bool.isRequired,
-  propFunc: PropTypes.func,
-  propNumber: PropTypes.number,
-  propString: PropTypes.string
-};
-
-App.defaultProps = {
-  name: 'React Application',
-  propArray: ['1', '2', '3', '4', '5'],
-  propBool: true,
-  propFunc: function (e) {
-    return e;
-  },
-  propNumber: 1,
-  propString: "String Value...."
-};
 
 export default App;
