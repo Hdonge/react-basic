c83892714ed10b8d5f2ee0d36409ecec75e402c7
Hdonge <harshal.donge@gmail.com>
2018-11-18 11:19:52 +0530

    React js Props Validation

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,11 +7,35 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>{this.props.headerText}</h1>
-        <h2>{this.props.content}</h2>
+        <h1>Hello, {this.props.name}</h1>
+        <h3>Array: {this.props.propArray}</h3>
+        <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
+        <h3>Func: {this.props.propFunc(3)}</h3>
+        <h3>Number: {this.props.propNumber}</h3>
+        <h3>String: {this.props.propString}</h3>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  name: PropTypes.string,
+  propArray: PropTypes.isRequired,
+  propBool: PropTypes.bool.isRequired,
+  propFunc: PropTypes.func,
+  propNumber: PropTypes.number,
+  propString: PropTypes.string
+};
+
+App.defaultProps = {
+  name: 'React Application',
+  propArray: ['1', '2', '3', '4', '5'],
+  propBool: true,
+  propFunc: function (e) {
+    return e;
+  },
+  propNumber: 1,
+  propString: "String Value...."
+};
 
 export default App;
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <div>
-        <App headerText="My Heading" content="The content is here....." />
+        <App />
     </div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
