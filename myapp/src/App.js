916a18b4eb9361bc271030199f7a0f86570e4f32
Hdonge <harshal.donge@gmail.com>
2018-11-29 12:49:11 +0530

    Working with list:collections

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,35 +2,33 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import Emp from './emp'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: ''
-    };
-
-    this.updateState = this.updateState.bind(this);
-  }
-
-  clearInput() {
-    this.setState({ data: '' });
-    ReactDOM.findDOMNode(this.refs.myText).focus();
-  }
-
-  updateState(e) {
-    this.setState({ data: e.target.value });
+  state = {
+    emps: [
+      { id: 101, name: 'Dipak', salary: 5000 },
+      { id: 102, name: 'Sush', salary: 6000 },
+      { id: 103, name: 'Roy', salary: 7000 },
+      { id: 104, name: 'Ham', salary: 8000 },
+      { id: 105, name: 'Dan', salary: 9000 }
+    ]
   };
 
   render() {
     return (
       <div>
-        <input type="text" value={this.state.data} onChange={this.updateState} ref="myText" />
-        <h4>{this.state.data}</h4>
-        <button onClick={this.clearInput.bind(this)}>Clear</button>
-      </div>
+        <table style={{ width: '500px', color: 'blue' }}>
+          <tbody>
+            {
+              this.state.emps.map((emp) => {
+                return (<Emp salary={emp.salary}>{emp.name}</Emp>)
+              })
+            }
+          </tbody>
+        </table>
+      </div >
     );
   }
 }
