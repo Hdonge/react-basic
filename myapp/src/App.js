17cf32c9ab33ba659fa66733fc0af32fa99727d4
Hdonge <harshal.donge@gmail.com>
2018-12-07 18:23:27 +0530

    React js routing

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,40 +1,30 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import logo from './logo.svg';
-import './App.css';
-import Emp from './emp'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './home';
+import Login from './login';
 
 class App extends Component {
-
-  state = {
-    emps: [
-      { id: 101, name: 'Dipak', salary: 5000 },
-      { id: 102, name: 'Sush', salary: 6000 },
-      { id: 103, name: 'Roy', salary: 7000 },
-      { id: 104, name: 'Ham', salary: 8000 },
-      { id: 105, name: 'Dan', salary: 9000 }
-    ]
-  };
-
-  delEmp = (index, e) => {
-    const copyemps = Object.assign([], this.state.emps);
-    copyemps.splice(index.id, 1);
-    this.setState({ emps: copyemps });
-  }
-
   render() {
     return (
-      <div>
-        <table style={{ width: '500px', color: 'blue' }}>
-          <tbody>
-            {
-              this.state.emps.map((emp) => {
-                return (<Emp salary={emp.salary} key={emp.id} deleteEvent={this.delEmp.bind(this, emp.id)}>{emp.name}</Emp>)
-              })
-            }
-          </tbody>
-        </table>
-      </div >
+      <Router>
+        <div>
+          <h2>Welcome to the React Router tutorial</h2>
+          <ul>
+            <li>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/login'}>Login</Link>
+            </li>
+          </ul>
+          <hr />
+
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
